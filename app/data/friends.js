bcf3974553375aae7d36d32ff7429458ec83586f
Davi Matos <davidiasmatos@gmail.com>
2017-05-07 18:55:29 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+        name: "Violet",
+        photo: "https://az616578.vo.msecnd.net/files/responsive/cover/main/desktop/2016/05/23/635996256145365654-1904239434_old-lady.jpg",
+        scores: [1,3,6,3,2,1,5,5,4,2]
+    }, {
+        name: "Rose",
+        photo:"http://www.50shadesofpink.ca/wp-content/uploads/2015/02/Mature-woman.jpg",
+        scores: [1,1,3,4,2,4,3,3,1,4]
+    }, {
+        name: "Mary",
+        photo: "http://countrysidechristian.info/wp-content/uploads/2016/09/iStock_cup-of-tea.jpg",
+        scores: [4,3,1,3,2,3,3,5,4,4]
+    }
+];
+
+module.exports = friends;
